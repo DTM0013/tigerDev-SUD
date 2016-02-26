d72354168e91097497edfc8b0163a4ba189dba71
DTM0013 <DTM0013@auburn.edu>
2016-02-26 12:55:58 -0600

    Organized and made a make file.

---
+++ libs/Database/MapBuilder.cpp
@@ -0,0 +1,92 @@
+/*
+* Class: MapBuilder
+* Description: Reads information about the map in
+*	from a txt file, and appropriatly build it.
+*
+* Author: David Moore
+* Date created: 2-22-2016
+* Last modified: 2-22-2016
+*
+*/
+
+//Include statements
+#include "Database/MapBuilder.h"
+//#include "Entity/Room.h"
+//#include "Entity/Room.cpp"
+#include <string>
+#include <vector>
+#include <iostream>
+
+/*
+* Default constructor
+*/
+MapBuilder::MapBuilder() {	
+};
+
+/*
+* General use constructor
+* Sets mapName
+*/
+MapBuilder::MapBuilder(std::string mapName) {
+	this->mapName = mapName;
+};
+
+/*
+* Returns the name of the txt file that is read in
+*/
+std::string MapBuilder::getMapName() {
+	return mapName;
+};
+
+/*
+* Returns the x pos of the start room
+*/
+int MapBuilder::getStartRoomXpos() {
+	return startRoomXpos;
+};
+
+/*
+* Returns the y pos of the start room
+*/
+int MapBuilder::getStartRoomYpos() {
+	return startRoomYpos;
+};
+
+/*
+* Returns the x pos of the end room
+*/
+int MapBuilder::getEndRoomXpos() {
+	return startRoomXpos;
+};
+
+/*
+* Returns the y pos of the end room
+*/
+int MapBuilder::getEndRoomYpos() {
+	return endRoomYpos;
+};
+
+/*
+* Return the room object with
+*	position [x][y]
+*/
+Room* MapBuilder::getRoom(int x, int y) {
+	return map[x][y];
+};
+
+/*
+* Reads in data from mapName
+* Sets start and end room x & y values
+* Sets numberOfRooms
+* Constructs the 2D vector of rooms
+* Returns true if the data was read in
+*	& the map was built
+* Returns false if the data could not be
+*	read in or the map could not be built
+*
+* Note: need to decide on format of txt file before making
+*/
+bool MapBuilder::buildMap() {
+	return false;
+};
+
---
+++ libs/Entity/Character.h
@@ -0,0 +1,190 @@
+/*
+* Class: Character Class Header
+* Description: Protypes the Character class
+* 
+* Author: David Moore
+* Date created: 2-17-2016
+* Last modified: 2-17-2016
+*
+*/
+
+//Header guard start
+#ifndef Character_H
+#define Character_H
+
+//Include statement
+#include "Entity.h"
+
+class Character {
+
+//Variables
+private:
+	/*
+	* Atributes akin to SPECIAL from Fallout 
+	* DO NOT CHANGE ATTRIBUTES ARRAY DURING CALCULATIONS
+	* Change currentAttribute for calculations
+	* Attributes subject to change
+	*
+	* 0 = strength melee damage & equip requiremet 
+	* 1 = dexterity slight melee damage & slight crit chance & equip requirement 
+	* 2 = constitution health & slight stamina
+	* 3 = agility Stamina & initative
+	* 4 = perception range weapons & crit chance
+	* 5 = intelligence level up speed or loot amount??? (no direct effect on stats
+	*/
+	int attributes[6];	
+	int currentAttributes[6];	
+
+	/**
+ 	* Attributes that will be manipulated in methods then assigned
+ 	* to their respective indexes in currentAttriubutes array.
+ 	*/
+ 	
+ 	int strengthAttr;
+ 	int dexterityAttr;
+ 	int constitutionAttr;
+ 	int agilAttr;
+ 	int perceptionAttr;
+ 	int intellAttr;
+
+	/*
+	* Stats akin to stats from Fallout
+	* Base stats calculated from Atributes
+	* DO NOT CHANGE DURING CALCULATIONS
+	* Change currentStats for calculations
+	* Subject to change
+	*
+	* 0 = melee
+	* 1 = ranged
+	* 2 = throwing 
+	* 3 = medicine
+	* 4 = maxWeight
+	* 5 = maxStamina
+	*/
+	int stats[6];
+	int currentStats[6];
+	
+	/*
+	* Stats that will be manipulated in methods then assigned
+	* to their respective indexes in currentStats array.
+	*/
+	int meleeStat;
+	int rangedStat;
+	int throwStat;
+	int medStat;
+	int maxWeightStat;
+	int maxStaminaStat;
+
+	/*
+	* Equiped items
+	* Array position corrisponds to an equip slot
+	* 0 = boots | 1 = legs/waist | 2 = chest/arms | 3 = gloves | 
+	* 4 = held in left hand | 5 = held in right hand | 6 = head |
+	*/
+	object* equipedItems[7];
+	
+//Functions
+protected:
+	/*
+	* Get method for Atributes
+	* value of attributeNumber specifies what attribute info to return
+	* returns the attribute's value
+	*/
+	int getAtribute(int attributeNumber);
+	
+	/*
+	* Get method for stats
+	* value of statNumber specifies what stat info to return
+	* returns the stat's value
+	*/
+	int getStat(int statNumber);
+	
+	/*
+	* Get method for current attribute values
+	* value of attributeNumber specifies what attribute info to return
+	* returns the current value of the attribute
+	*/
+	int getCurrentAttribute(int attributeNumber);
+	
+	/**
+	 * Get method for entire currentAttribute array
+	 * returns currentAttribute array
+	 */
+	 int getCurrentAttributeArray(int attrArray[6]);
+	
+	/*
+	* Get method for current stat values
+	* value of statNumber specifies what stat info to return
+	* returns the current value of the attribute
+	*/
+	int getCurrentStat(int statNumber);
+	
+	/**
+	 * Get method for entire currentStat array
+	 * Returns currentStat array
+	 */
+	 int getCurrentStatArray(int statArray[6]);
+
+	/*
+	* Sets a new value for a current attribute
+	* The attribute will change to the value specified by amount
+	* returns the new value of the attribute
+	*/
+	int setCurrentAttribute(int attributeNumber, int amount);
+	
+	/**
+	 * Method sets new currentAttribute aray
+	 *returns new currentAttribute array
+	 */
+	 int setCurrentAttributeArray(int attrArray[6]);
+	
+	/*
+	* Sets a new value for a current stat
+	* The stat will change to the value specified by amount
+	* returns the new value of the stat
+	*/
+	int setCurrentStat(int statNumber, int amount);
+	
+	/**
+	 * Method sets new currentStat array
+	 * returns new currentStat array
+	 */
+	int setCurrentStatArray(int statArray[6]);
+	
+	/**
+	 * Method used to distribute attribute points at the beginning
+	 * of character creation or when character levels up
+	 */
+	 int distributeAttributePoints(int attrArray[6]);
+	 
+	 /**
+	  * Method used to distribute stat points at the
+	  * begininng of character creation or when character levels
+	  * up.
+	  */
+	  int distributeStatPoints(int skillArray[6]);
+	
+	/*
+	* Gets the currently equiped item
+	* returns the item in slotNumber
+	* returns null if no item is in slotNumber
+	*/
+	Item getItem(int slotNumber)
+	
+	/*
+	* Equips the item specified by Item to equip
+	* returns true if the item was equiped
+	*/
+	bool equipItem(Object itemToEquip);
+
+//Constructor
+public:
+	/*
+	* Default constructor
+	* Should have no Character objects
+	*/
+	Chracter();
+
+};
+//Header guard end
+#endif
